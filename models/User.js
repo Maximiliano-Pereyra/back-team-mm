b7ee17ce4d888510144214ab75b9f0123925ce6c
Maximiliano Pereyra <maxigenio99@gmail.com>
2022-11-10 19:26:21 -0300

    modelo de hotel

--- models/User.js
+++ models/User.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose')
 
 const schema = new mongoose.Schema({
-    name: {type: String ,require: true},
-    lastName: {type: String ,require: true},
-    role: {type: String ,require: true},
-    photo: {type: String ,require: true},
-    age: {type: Number ,require: true},
-    email: {type: String ,require: true},
-    password: {type: String ,require: true},
-    code: {type: String ,require: true},
-    verified: {type: Boolean ,require: true},
-    logged: {type: Boolean,require: true},
+    name: {type: String ,required: true},
+    lastName: {type: String ,required: true},
+    role: {type: String ,required: true},
+    photo: {type: String ,required: true},
+    age: {type: Number ,required: true},
+    email: {type: String ,required: true},
+    password: {type: String ,required: true},
+    code: {type: String ,required: true},
+    verified: {type: Boolean ,required: true},
+    logged: {type: Boolean,required: true},
 })
 
 const Admin = mongoose.model('users',schema)
