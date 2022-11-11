d2b91fee167fd572ca04b5c1f651279121480b66
AlvarezMariaBelen <beluchis1991@gmail.com>
2022-11-11 17:30:55 -0300

    definicion del modelo itinerary

---
+++ controllers/itinerary.js
@@ -0,0 +1,9 @@
+const Itinerary = require('../models/Itinerary')
+
+const controller = {
+
+    create: async(req, res) => {}
+
+}
+
+module.exports = controller
--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ let router = require('express').Router()
 //requiero las rutas de cada modelo a controlar
 let user = require('./user')//ubicacion rutas user
 let city = require('./city')
+let itinerary = require('./itinerary')
 
 //obligo al enrutador principal que use la palabra user para controlar las rutas de user
 router.use('/user',user)
 router.use('/city',city)
+router.use('/itinerary',itinerary)
 
 module.exports = router;
