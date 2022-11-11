bdb3a7fb355005e5293d1b24fc756bee6617d09f
Maximiliano Pereyra <maxigenio99@gmail.com>
2022-11-11 02:53:41 -0300

    creacion de modelo de show

--- models/Hotel.js
+++ models/Hotel.js
@@ -5,7 +5,7 @@ const schema = new mongoose.Schema({
     photo: [{type: String ,required: true}],
     capacity: {type: Number ,required: true},
     cityId: {type: mongoose.Types.ObjectId, ref: 'cities', required: true},
-    userId: {type: mongoose.Types.ObjectId, ref:'admins', required: true}
+    userId: {type: mongoose.Types.ObjectId, ref:'users', required: true}
 })
 
 const Hotel = mongoose.model('hotels',schema)
