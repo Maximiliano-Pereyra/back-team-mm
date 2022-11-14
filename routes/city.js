d03ccf317eed9ee42184589dfc89b723a5e66f48
AlvarezMariaBelen <beluchis1991@gmail.com>
2022-11-14 02:29:51 -0300

    cambios axios y rutas

--- routes/city.js
+++ routes/city.js
@@ -2,8 +2,8 @@ let router = require("express").Router();
 
 let { create, read, readOne, update, destroy } = require("../controllers/city");
 
-router.route("/create").post(create);
-router.route("/read").get(read);
+router.post("/", create);
+router.get("/", read);
 router.get("/:id", readOne);
 router.put("/update/:id", update);
 router.delete("/destroy/:id", destroy);
