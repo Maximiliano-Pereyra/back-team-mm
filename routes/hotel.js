e8a8cbbccaa7c125f18aba12eea867aebfe3fdf1
Maximiliano Pereyra <maxigenio99@gmail.com>
2022-11-22 01:48:08 -0300

    controles de hotels

--- routes/hotel.js
+++ routes/hotel.js
@@ -2,7 +2,7 @@ let router = require('express').Router()
 const schema = require('../schemas/hotel')
 const validatorHotel = require('../middlewares/validadorHotel')
 
-let {create, read, update, destroy} = require('../controllers/hotel')
+let {create, read, update, destroy, readOne} = require('../controllers/hotel')
 
 router.route('/')
     .get(read)
@@ -10,5 +10,6 @@ router.route('/')
 
 router.patch('/:id',update)
 router.delete('/:id',destroy)
+router.get("/:id", readOne);
 
 module.exports = router;
